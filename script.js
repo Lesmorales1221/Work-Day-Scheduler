06feec61f8cbe357659b04d5ff1b89f1c5c8fd89
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-04-04 19:30:49 -0400

    added the timeblockas and the script in java

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// Var for hour
-var nineam = $('#9am');
-var tenam = $('#10am'); 
-var elevenam = $('#11am')
+//current day is displayed at the top of the calendar
+$('#currentDay').text(moment().format("MMM Do YY"))
+
+
+//each time block is color-coded to indicate whether it is in the past, present, or future
+
+//WHEN I click into a time block
+// THEN I can enter an event
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
