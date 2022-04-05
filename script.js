09308f6ff86f94f3f49f86fa268f095d5d94aa22
Leslie Morales <lesliemorales@Leslies-MacBook-Pro.local>
2022-04-05 18:29:20 -0400

    added console.log(this) for save button

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 //current day is displayed at the top of the calendar
-$('#currentDay').text(moment().format("MMM Do YY"))
+$('#currentDay').text(moment().format("MMM Do YY"));
 
 
 //each time block is color-coded 
 //to indicate whether it is in the past, present, or future
 function colortimeblock() {
     var hour = moment().hour();
-}
+
 
 $('time-block').each(function() {
     var currenthour =parseInt($(this).attr('id'));
-})
 
 //this should be a function that runs and checks a conditioal (if statement)
-if (currenthour > hour) {
-        $(this).addclass('future');
-} else if (currenthour === hour) {
-    $(this).addclass('present');
-} else {
-    $(this).addclass('past');
-}
 
+     if (currenthour > hour) {
+        $(this).addclass('future');
+        } else if (currenthour === hour) {
+        $(this).addclass('present');
+         } else {
+        $(this).addclass('past');
+        }
+    })  
+};
 
 //WHEN I click into a time block
 saveBtn.on('click', function() {
@@ -29,10 +30,15 @@ saveBtn.on('click', function() {
 var time = $(this).siblings('.hour').text();
 var plan = $(this).siblings('.plan').val();
 
+// THEN the text for that event is saved in local storage
+
 });
 
 // THEN I can enter an event
 // WHEN I click the save button for that time block
 // THEN the text for that event is saved in local storage
+
+
+
 // WHEN I refresh the page
 // THEN the saved events persist
